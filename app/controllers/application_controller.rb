b5831ef3d93c7a07a1428d9fc18b51274b0c96df
Chadwick Flores <chadwickflores@gmail.com>
2016-10-05 14:08:39 -0500

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
+
+  def authorize
+    unless current_user
+      redirect_to sign_in_path
+    end
+  end
+end
